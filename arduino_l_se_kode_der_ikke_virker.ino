5b34fdbf9fc3febbceb85362291108f465b8712a
xxBreadManzz <82200744+xxBreadManzz@users.noreply.github.com>
2021-04-09 08:40:00 +0200

    hmm

---
+++ arduino_l_se_kode_der_ikke_virker.ino
@@ -0,0 +1,16 @@
+void setup() {
+int pass1, pass2, pass3;
+pass1 = 4;
+pass2 = 2;
+pass3 = 0;
+}
+
+void loop() {
+ int pass1, pass2,pass3;
+printf("enter password:");
+scanf("%d", pass1);
+scanf("%d", pass2);
+scanf("%d", pass3); 
+
+if 
+}
