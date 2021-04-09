50ec52451d4d51109ea25273118812628496ddd6
xxBreadManzz <82200744+xxBreadManzz@users.noreply.github.com>
2021-04-09 08:33:32 +0200

    Add files via upload

---
+++ password/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number;
+
+    printf("enter password:");
+    scanf("%d", &number);
+
+    if (number==69) {
+        printf("nice");
+    }
+
+    else {
+        printf("wrong password");
+    }
+return 0;
+}
